10f2d64764435a9e5cc8568bc7d37e985c86c149
LocalHostDiluk <169072534+LocalHostDiluk@users.noreply.github.com>
2025-04-08 17:04:39 -0600

    Update productController.js

--- src/contoller/productController.js
+++ src/contoller/productController.js
@@ -11,6 +11,23 @@ export const getProducts = async (req, res) => {
   }
 };
 
+export const getProductById = async (req, res) => {
+  try {
+    const { id } = req.params; // Obtener el ID desde los parámetros de la URL
+    const product = await Product.findByPk(id); // Buscar el producto por su ID
+
+    if (!product) {
+      return res.status(404).json({ error: "Producto no encontrado" });
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    console.error("Error al obtener el producto:", error);
+    res.status(500).json({ error: "Error al obtener el producto" });
+  }
+};
+
+
 export const createProduct = async (req, res) => {
   const { nombre, precio, stock, descripcion, imagen, categoria } = req.body;
 
