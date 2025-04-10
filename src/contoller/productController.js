5dffbacb895e876344e047d9a9f60640adef4e35
LocalHostDiluk <169072534+LocalHostDiluk@users.noreply.github.com>
2025-04-10 01:38:42 -0600

    Update productController.js

--- src/contoller/productController.js
+++ src/contoller/productController.js
@@ -46,10 +46,6 @@ export const createProduct = async (req, res) => {
   }
 
   try {
-    const categoriaExists = await Categoria.findByPk(categoria_id);
-    if (!categoriaExists) {
-      return res.status(400).json({ message: "La categoría especificada no existe" });
-    }
     const newProduct = await Product.create({
       nombre,
       precio,
